1a17545c1296aed55f41dfddf90e77bd19ba28d6
mohammedafrin <mohammedafrin1974@gmail.com>
2024-06-04 13:19:17 +0530

    second commit

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -4,6 +4,9 @@ export const sendToken = (user, statusCode, res, message) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
+    httpOnly:true,
+    secure:true,
+    sameSite:"None",
     // Set httpOnly to true
   };
 
